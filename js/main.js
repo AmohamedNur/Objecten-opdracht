29779d740e8acabf2e291e6b9956d4f6ce0db5af
AmohamedNur <302881042@student.rocmondriaan.nl>
2022-05-30 14:48:10 +0200

    object toegevoegd

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 console.log('geladen');
 
-const data = [
-    {
-        titel:'John Doe',
-        discription:'Software Developer',
-        Image:"https://avatar1.png"
-    }
-];
+const container = document.querySelector('.container');
+
+let html = '<div class="card">';
+
+for (let i = 0; i < 2; i++) {
+    const object = data[i];
+
+    html += '<div class="card">'
+    html += `<h4>John Doe">${object.title}</h4>`;
+    html += `<p>software developer">${object.discription}</p>`;
+    html += `<<img src="${object.Image}" alt="Avatar"`;
+    html += '</div>';
+
+}
+
+
+html += '</div>';
+
+container.innerHTML += html;
---
+++ js/object.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        titel:'John Doe',
+        discription:'Software Developer',
+        Image:"https://avatar1.png"
+    },
+
+    {
+        titel:'Mula mus',
+        discription:'Engineer',
+        Image:"https://avatar2.png"
+    },
+
+    {
+        titel:'Amigoo',
+        discription:'Raper',
+        Image:"https://avatar1.png"
+    }
+];
